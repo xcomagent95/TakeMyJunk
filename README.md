3c08c1e2b89efbb51d6c29aa43d53d9c81ecc319
xcomagent95 <alex.pilz95@googlemail.com>
2022-03-30 16:38:37 +0200

    Very Basic Server Setup
    Can host a simple

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# takeMyJunk
- Repository for the IfGi 2022 Hackathon
-https://docs.google.com/document/d/1T2tPPxKd4clyt1WB2nrwditWjaVHxc7aOUX3FzxX61c/edit?usp=sharing
+# hackathon
+ A repositoty for the IfGi hack 2022
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+
+const port = 3000; //define port via which the application will be accessable
+const bodyParser = require('body-parser'); 
+
+//Parser for Requests
+app.use(express.json()); 
+app.use(express.urlencoded());
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+
+//Folders
+app.use(express.static(__dirname + '/html')); //define html folder
+
+//Gets for webpages to be hosted
+app.get("/test", (req, res) => { res.sendFile(__dirname + "/html/testpage.html"); });

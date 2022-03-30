196e10acfd5ca46851e4c87caa482dc185551410
GitLiliana <l_gits01@uni-muenster.de>
2022-03-30 19:08:06 +0200

    basic html page+css, add map, add icon

---
+++ html/map.js
@@ -0,0 +1,7 @@
+// Initialize the map and set coordinates and zoom
+var map = L.map('map').setView([51.96, 7.63], 11);
+
+// Add tile layer with map from OpenStreetMap
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);

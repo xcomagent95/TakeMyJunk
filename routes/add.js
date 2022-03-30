86a1b9eb0bc21139bfd12458816c4e2191f59881
xcomagent95 <alex.pilz95@googlemail.com>
2022-03-30 17:53:31 +0200

    Add Things to DB
    Add some Features to DB

---
+++ routes/add.js
@@ -0,0 +1,38 @@
+"use strict"
+
+var express = require('express'); //require express
+const app = express(); //initialize express app
+var router = express.Router(); //initialize express-router
+
+//Here we are configuring express to use body-parser as middle-ware
+app.use(express.json());
+app.use(express.urlencoded());
+
+//MongoClient and DB
+const url = 'mongodb://localhost:27017' // connection URL for local use
+const dbName = 'takemyjunk' // database name
+const boxesCollection = 'boxes' // collection containing the locations
+const MongoClient = require('mongodb').MongoClient;
+const { stringify } = require('querystring');
+const client = new MongoClient(url) // mongodb client
+
+//Post Location - this post operation can be used to store new locations in the locations collection
+router.post('/newBox', function (req, res, next) {
+
+
+
+  var test = req.body.name
+  console.log(test)
+  client.connect(function(err) 
+  {
+    const db = client.db(dbName) //database
+    const collection = db.collection(boxesCollection) //locations collection
+    collection.insertOne({test}, function(err, result) //insert new location into collection
+          {
+            //res.sendFile(__dirname + "../html/testpage.html"); //send positive response -> the post operation war successful
+            return;
+           })
+  }) 
+});
+
+module.exports = router; //export as router

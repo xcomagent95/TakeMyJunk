6f40afeadfd4e4c1fbbc745bd9b7773e5af536fc
jona159 <raabejonas@yahoo.de>
2022-03-31 11:41:30 +0200

    leaflet-draw

--- html/map.js
+++ html/map.js
@@ -1,7 +1,42 @@
 // Initialize the map and set coordinates and zoom
-var map = L.map('map').setView([51.96, 7.63], 12);
+var map = L.map('map', {drawControl: true}).setView([51.96, 7.63], 12);
 
 // Add tile layer with map from OpenStreetMap
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+var popup_content = 'Hallo welt';
+var drawnItems = new L.FeatureGroup().addTo(map);
+var drawControl = new L.Control.Draw({
+         draw: {
+             polygon: false,
+             circle:false,
+             circlemarker:false,
+             polyline:false,
+             rectangle:false
+         },
+          edit: {
+              featureGroup: drawnItems
+          }
+      });
+      drawControl.addTo(map);
+
+      map.on('draw:created', function (e) {
+        var tempMarker = e.layer.addTo(map);
+               
+    
+        //   if (type === 'marker') {
+            
+        //   }
+    
+        // Do whatever else you need to. (save to db, add to map etc)
+        map.addLayer(tempMarker);
+    });
+    
+
+    
+
+    map.on('click', function(e){
+
+    })

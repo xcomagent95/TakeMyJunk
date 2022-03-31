6aafa13c56d72091fc16f61064853b176e68a04b
xcomagent95 <alex.pilz95@googlemail.com>
2022-03-31 15:29:00 +0200

    switch Coordinates
    add individual coordinates to json

--- routes/add.js
+++ routes/add.js
@@ -20,21 +20,27 @@ const client = new MongoClient(url) // mongodb client
 router.post('/addBox', function (req, res, next) {
 
   console.log("Submitted a Box!")
+  console.log(req.body)
 
   var name = req.body.name
   var commentary = req.body.commentary
   var date = req.body.date
   var street = req.body.street
   var house_number = req.body.house_number
+  var coordinates = req.body.coordinates
+  console.log(coordinates);
+
   var items = JSON.parse('[' + req.body.items.slice(0, -1) + ']')
 
   var box = '{"type": "FeatureCollection","features": [{"type": "Feature","properties": {"commentary":"' + commentary + 
   '", "date":"' + date + 
   '", "street":"' + street + 
   '", "house_number":"' + house_number + 
-  '"},"geometry": {"type": "Point","coordinates": [7.6110899448394775,51.96942238609061]}}]}'
+  '"},"geometry": {"type": "Point","coordinates": '+coordinates+'}}]}'
 
-  console.log(box)
+  console.log("box");
+  console.log(box);
+
   var obj = JSON.parse(box);
   //Check if Name exists
   client.connect(function(err) 
